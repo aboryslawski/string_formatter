b26d0350ea6f82ca6321e1be5c3beb825f8cde4f
laocane <adamboryslawski97@gmail.com>
2020-06-17 15:07:31 +0200

    VB6 compiler max line breaks number is now taken into consideration.

--- newline.py
+++ newline.py
@@ -2,12 +2,19 @@ from itertools import islice
 import pyperclip
 
 name_of_the_file = pyperclip.paste()
-def format_text(name_of_the_file):    
+def format_text(name_of_the_file):
+    counter = 0
     array = name_of_the_file.split("\n")   
-    final_string = 'SQLstring =  _ \n "' + array[0] + '"'
+    final_string = 'strKomenda1 =  _ \n "' + array[0] + '" & vbCrLf _'
     for i in islice(array, 1, None):
         if not i == '':
-            final_string = final_string + '\n& "' + i + '" _'
+            final_string = final_string + '\n&"' + i 
+            if not counter == 11:
+                final_string = final_string + '" & vbCrLf _'
+            if counter == 11:
+                final_string = final_string + '\n\n' + 'strKomenda1 = strKomenda1 & vbCrLf _ ' 
+                counter = 0
+            counter = counter + 1
     if len(array) > 1:
         final_string = final_string[:-1]
     print(final_string)
